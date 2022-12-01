eadd2a6f60ebb98ec441d50a0c8250c2487864a1
haegon <claudiopereiradev@gmail.com>
2022-12-01 22:44:52 +0000

    Small ugrades.

--- Day 1/adv1-2022.py
+++ Day 1/adv1-2022.py
@@ -4,9 +4,9 @@ def main():
     data = open('adv1-2022.txt', 'r')
     elfs = data.read().strip().split('\n\n')
     for meals in elfs:
-        sum = sum([int(num) for num in meals.split('\n')])
-        l.append(sum)
-    print(f'Part 1 value: {sorted(l)[-1]}')
+        sum_l = sum([int(num) for num in meals.split('\n')])
+        l.append(sum_l)
+    print(f'Part 1 value: {max(l)}')
     print(f'Part 2 value: {sum(sorted(l)[len(l)-3:])}')
 
 if __name__ == '__main__':
