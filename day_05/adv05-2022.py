bb3ebf7c9ab1bb28467f6fe45f3ac922422d5d1f
haegon <claudiopereiradev@gmail.com>
2022-12-08 03:17:50 +0000

    Formatting day 5.

--- day_05/adv05-2022.py
+++ day_05/adv05-2022.py
@@ -1,9 +1,7 @@
 import re
 
 stacks = []
-
 find_numbers = "([0-9]+)"
-
 data = open('adv05-2022.txt', 'r')
 
 # Get the stacks in a list of lists format.
@@ -17,15 +15,15 @@ stacks.pop()
 # Remove empty containers, invert the columns and return as list of lists again.
 stacks = ([list(''.join(col).strip()[::-1]) for col in zip(*stacks)])
 
-# for line in data:
-#     a,b,c = map(int,(re.findall(find_numbers, line)))
-#     stacks[c-1].extend(stacks[b-1][-a:][::-1])
-#     stacks[b-1] = stacks[b-1][:-a]
-# print('Part 1 result:',''.join([r[-1] for r in stacks]))
-
 for line in data:
     a,b,c = map(int,(re.findall(find_numbers, line)))
-    stacks[c-1].extend(stacks[b-1][-a:])
+    stacks[c-1].extend(stacks[b-1][-a:][::-1])
     stacks[b-1] = stacks[b-1][:-a]
-print('Part 2 result:',''.join([r[-1] for r in stacks]))
+print('Part 1 result:',''.join([r[-1] for r in stacks]))
+
+# for line in data:
+#     a,b,c = map(int,(re.findall(find_numbers, line)))
+#     stacks[c-1].extend(stacks[b-1][-a:])
+#     stacks[b-1] = stacks[b-1][:-a]
+# print('Part 2 result:',''.join([r[-1] for r in stacks]))
 
